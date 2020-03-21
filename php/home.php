33ac80945443a4421c5d6b2bc78136ae0df7bc38
code-bandit <ripudamansingh1807@gmail.com>
2020-03-21 19:33:01 +0530

    home.php and login.php edited

--- php/home.php
+++ php/home.php
@@ -1,3 +1,19 @@
 <?php
     echo "Home"
 ?>
+
+<!DOCTYPE html>
+<html lan="en">
+
+<head>
+    
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    
+</head>
+
+<body>
+</body>
+
+</html>
--- php/login.php
+++ php/login.php
@@ -40,7 +40,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Sign Up</title>
+    <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         body{ font: 14px sans-serif; }
