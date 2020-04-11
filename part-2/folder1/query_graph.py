a9be6add37baaeb350a37ca546f9ab3924cd7bc2
PagillaMeghanaReddy <48548123+PagillaMeghanaReddy@users.noreply.github.com>
2020-04-11 13:23:25 +0530

    Update query_graph.py

--- part-2/folder1/query_graph.py
+++ part-2/folder1/query_graph.py
@@ -1,62 +1,47 @@
 from py2neo import Graph
-
-#####################################################################
-# Graph database config
-#####################################################################
-
-# Set up a link to the local graph database.
-# Ideally get password from ENV variable
-# graph = Graph(getenv("NEO4J_URL"), auth=(getenv("NEO4J_UID"), getenv("NEO4J_PASSWORD")))
-#graph = Graph("bolt://127.0.0.1:7687", auth=('neo4j', 'neo4j'))
-graph = Graph("bolt://localhost:7687", auth=('neo4j', 'saisantosh'))
-
-
-
-
-
-
+graph = Graph("bolt://localhost:7687", auth=('neo4j', 'abc'))
 query='''
 	MATCH (a:Crop{name:{crop_name}})
 	RETURN a.name
 	'''
-cursor = graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
+cropname = graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
 
-print(cursor)
+print(cropname)
 
 print("Getting states where the crop grown:")
 
 query=''' MATCH(a: Crop{name:{crop_name}}) -[:MAJORLY_GROWN_IN]->(b:state)
 		RETURN b.name  '''
-cursor= graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
-print(cursor)
+states= graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
+print(states)
 
 print("Getting diseases that can occur:")
 
 query=''' MATCH(a: Crop{name:{crop_name}}) -[:DISEASES_MAY_OCCUR]->(b:disease_may_occur)
 		RETURN b.name  '''
-cursor = graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
-print(cursor)
+diseases = graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
+print(diseases)
 
 print("Getting ph of soil that is sutibale for growing:")
 query=''' MATCH(a: Crop{name:{crop_name}}) -[:SUITABLE_SOIL_PH]->(b:suitable_soil_ph)
 		RETURN b.name  '''
-cursor= graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
-print(cursor)
+soilph= graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
+print(soilph)
 
 print("Getting required equipments for given soil:")
 query=''' MATCH(a: Crop{name:{crop_name}}) -[:EQUIPMENT_REQUIRED]->(b:required_equipment)
 		RETURN b.name  '''
-cursor = graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
-print(cursor)
+equipment = graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
+print(equipment)
 
 print("Getting fertilizers required:")
 query=''' MATCH(a: Crop{name:{crop_name}}) -[:FERTILIZERS_REQUIRED]->(b:required_fertilizers)
 		RETURN b.name  '''
-cursor = graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
-print(cursor)
+fertilizers = graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
+print(fertilizers)
 
 print("Getting required soil type:")
 query=''' MATCH(a: Crop{name:{crop_name}}) -[:SUITABLE_SOIL_TYPE]->(b:soiltype)
 		RETURN b.name  '''
-cursor = graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
-print(cursor)
+soiltype = graph.run(query, parameters={'crop_name': 'paddy'}).evaluate()
+print(soiltype)
