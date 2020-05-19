4a70941c0c288cf97e3b7d7144c45ea094f1c05d
PagillaMeghanaReddy <48548123+PagillaMeghanaReddy@users.noreply.github.com>
2020-05-19 17:40:18 +0530

    Add files via upload

--- part-2/folder2/extract_data.py
+++ part-2/folder2/extract_data.py
@@ -2,16 +2,20 @@ from py2neo import Graph
 import csv
 
 #Connection to graph database
-graph = Graph("bolt://localhost:7687", auth=('neo4j', 'abc'))
+graph = Graph("bolt://localhost:7687", auth=('neo4j', 'saisantosh'))
 
 # Adding uniqueness constraints.
 graph.run("CREATE CONSTRAINT ON (c:Crop) ASSERT c.name IS UNIQUE;")
 graph.run("CREATE CONSTRAINT ON (c:state) ASSERT c.name IS UNIQUE;")
+graph.run("CREATE   CONSTRAINT ON (t:temp) ASSERT t.name IS UNIQUE;")#
 graph.run("CREATE CONSTRAINT ON (s:soiltype) ASSERT s.name IS UNIQUE;")
-graph.run("CREATE CONSTRAINT ON (m:suitable_soil_ph) ASSERT m.name IS UNIQUE;")
+graph.run("CREATE CONSTRAINT ON (w:weather) ASSERT w.name IS UNIQUE;")#
+# graph.run("CREATE CONSTRAINT ON (m:suitable_soil_ph) ASSERT m.name IS UNIQUE;")
 graph.run("CREATE CONSTRAINT ON (p:required_equipment) ASSERT p.name IS UNIQUE;")
 graph.run("CREATE CONSTRAINT ON (p:required_fertilizers) ASSERT p.name IS UNIQUE;")
 graph.run("CREATE CONSTRAINT ON (p:disease_may_occur) ASSERT p.name IS UNIQUE;")
+
+
 #Neo4j query for constructing node for crops
 def process_crop_data(crop_data):
     query = """
@@ -32,6 +36,16 @@ def process_statewise_data(state_data):
        """
     run_neo_query(state_data,query)
 
+
+def process_temp_type_data(temp_data):
+    query = """
+           UNWIND {rows} AS row
+           MERGE (crop:Crop {name:row.cropname})
+           MERGE (temp1:temp {name:row.attemp})
+           MERGE (crop)-[:GROWN_AT_TEMP]->(temp1)
+       """
+    run_neo_query(temp_data,query)
+
 #Neo4j query for constructing node soil type and adding the relation between  crop and soil
 def process_soil_type_data(soil_data):
     query = """
@@ -43,15 +57,26 @@ def process_soil_type_data(soil_data):
     run_neo_query(soil_data,query)
 
 
-#Neo4j query for constructing node ph and adding the relation between  ph  and crop
-def process_ph_type_data(ph_data):
+def process_weather_type_data(weather_data):
     query = """
             UNWIND {rows} AS row
             MERGE (crop:Crop {name:row.cropname})
-            MERGE (sr:suitable_soil_ph {name:row.ph})
-            MERGE (crop)-[:SUITABLE_SOIL_PH]->(sr)
+            MERGE (sr:weather {name:row.weather})
+            MERGE (crop)-[:SUITABLE_WEATHER_TYPE]->(sr)
         """
-    run_neo_query(ph_data,query)
+    run_neo_query(weather_data,query)
+
+
+
+#Neo4j query for constructing node ph and adding the relation between  ph  and crop
+# def process_ph_type_data(ph_data):
+#     query = """
+#             UNWIND {rows} AS row
+#             MERGE (crop:Crop {name:row.cropname})
+#             MERGE (sr:suitable_soil_ph {name:row.ph})
+#             MERGE (crop)-[:SUITABLE_SOIL_PH]->(sr)
+#         """
+#     run_neo_query(ph_data,query)
     
 #Neo4j query for constructing node equipment and relation between  crop and equipment
     
@@ -91,7 +116,7 @@ def run_neo_query(data, query):
 
 if __name__== "__main__":
     l=[]
-    with open('survey_results_public_agri.csv') as csv_file:
+    with open('test.csv') as csv_file:
     	csv_reader=csv.reader(csv_file,delimiter=',')
     	line_count=0
     	for row in csv_reader:
@@ -103,11 +128,11 @@ if __name__== "__main__":
     			this_dict["Instate"]=row[1]
     			this_dict["attemp"]=row[2]
     			this_dict["soilcondition"]=row[3]
-    			this_dict["ph"]=row[4]
-    			this_dict["weather"]=row[5]
-    			this_dict["equipment"]=row[6]
-    			this_dict["fertilizers"]=row[7]
-    			this_dict["diseases"]=row[8]
+    			# this_dict["ph"]=row[4]
+    			this_dict["weather"]=row[4]
+    			this_dict["equipment"]=row[5]
+    			this_dict["fertilizers"]=row[6]
+    			this_dict["diseases"]=row[7]
     			l.append(this_dict)
     		line_count+=1
     	# print(l)
@@ -115,7 +140,7 @@ if __name__== "__main__":
              
     	process_crop_data(l)
     l=[]
-    with open('survey_results_public_agri.csv') as csv_file:
+    with open('test.csv') as csv_file:
         csv_reader=csv.reader(csv_file,delimiter=',')
         line_count=0
         for row in csv_reader:
@@ -130,7 +155,7 @@ if __name__== "__main__":
         # print(l)
         process_statewise_data(l)
     l=[]
-    with open('survey_results_public_agri.csv') as csv_file:
+    with open('test.csv') as csv_file:
         csv_reader=csv.reader(csv_file,delimiter=',')
         line_count=0
         for row in csv_reader:
@@ -145,7 +170,7 @@ if __name__== "__main__":
         # print(l)#sending data required for insertion into graph crop details
         process_soil_type_data(l)
     l=[]
-    with open('survey_results_public_agri.csv') as csv_file:
+    with open('test.csv') as csv_file:
         csv_reader=csv.reader(csv_file,delimiter=',')
         line_count=0
         for row in csv_reader:
@@ -154,13 +179,13 @@ if __name__== "__main__":
             else:
                 this_dict={}
                 this_dict["cropname"]=row[0]
-                this_dict["ph"]=row[4]
+                this_dict["attemp"]=row[2]
                 l.append(this_dict)
             line_count+=1
         # print(l)#sending data required for insertion into graph crop details
-        process_ph_type_data(l)
+        process_temp_type_data(l)
     l=[]
-    with open('survey_results_public_agri.csv') as csv_file:
+    with open('test.csv') as csv_file:
         csv_reader=csv.reader(csv_file,delimiter=',')
         line_count=0
         for row in csv_reader:
@@ -169,13 +194,13 @@ if __name__== "__main__":
             else:
                 this_dict={}
                 this_dict["cropname"]=row[0]
-                this_dict["equipment"]=row[6]
+                this_dict["equipment"]=row[5]
                 l.append(this_dict)
             line_count+=1
         # print(l)#sending data required for insertion into graph crop details
         process_equipment_type_data(l)
     l=[]
-    with open('survey_results_public_agri.csv') as csv_file:
+    with open('test.csv') as csv_file:
         csv_reader=csv.reader(csv_file,delimiter=',')
         line_count=0
         for row in csv_reader:
@@ -184,13 +209,13 @@ if __name__== "__main__":
             else:
                 this_dict={}
                 this_dict["cropname"]=row[0]
-                this_dict["fertilizers"]=row[7]
+                this_dict["fertilizers"]=row[6]
                 l.append(this_dict)
             line_count+=1
         # print(l)#sending data required for insertion into graph crop details
         process_fertilizer_type_data(l)
     l=[]
-    with open('survey_results_public_agri.csv') as csv_file:
+    with open('test.csv') as csv_file:
         csv_reader=csv.reader(csv_file,delimiter=',')
         line_count=0
         for row in csv_reader:
@@ -199,7 +224,22 @@ if __name__== "__main__":
             else:
                 this_dict={}
                 this_dict["cropname"]=row[0]
-                this_dict["diseases"]=row[8]
+                this_dict["weather"]=row[4]
+                l.append(this_dict)
+            line_count+=1
+        # print(l)#sending data required for insertion into graph crop details
+        process_weather_type_data(l)
+    l=[]
+    with open('test.csv') as csv_file:
+        csv_reader=csv.reader(csv_file,delimiter=',')
+        line_count=0
+        for row in csv_reader:
+            if line_count==0:
+                print("the attributes section")
+            else:
+                this_dict={}
+                this_dict["cropname"]=row[0]
+                this_dict["diseases"]=row[7]
                 l.append(this_dict)
             line_count+=1
         # print(l)#sending data required for insertion into graph crop details
